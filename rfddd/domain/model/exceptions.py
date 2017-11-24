f3d9ee83cec53562505ebb8cfd35bc2f506023fd
kaelzhang81 <zhangy1981@qq.com>
2017-11-24 10:50:15 +0800

    Create exceptions.py

---
+++ rfddd/domain/model/exceptions.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+class NotDeclaredArgsException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(NotDeclaredArgsException, self).__init__(
+            'No arguments declared in __init__'
+        )
+
+
+class ArgWithoutValueException(Exception):
+    pass
+
+
+class CannotBeChangeException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(CannotBeChangeException, self).__init__(
+            'You cannot change values from a Value Object, create a new one'
+        )
+
+
+class ViolatedInvariantException(Exception):
+    pass
+
+
+class InvariantReturnValueException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(InvariantReturnValueException, self).__init__(
+            'Invariants must return a boolean value'
+        )
