0424d87185a78f4e8fd40a0affc3c05c749e4904
kaelzhang81 <zhangy1981@qq.com>
2017-11-25 11:44:59 +0800

    Update cargo_factory.py

--- rfddd/domain/model/cargo_factory.py
+++ rfddd/domain/model/cargo_factory.py
@@ -2,9 +2,9 @@
 """the implement of factory"""
 
 import abc
-from factory import Factory
-from delivery import Delivery
-from cargo import Cargo
+from .factory import Factory
+from .delivery import Delivery
+from .cargo import Cargo
 
 
 class CargoFactory(object):
@@ -13,15 +13,15 @@ class CargoFactory(object):
 
     def create(self, cargo_id, days):
         delivery = Delivery(days);
-        return Cargo(delivery, cargo_id);
+        return Cargo(cargo_id, delivery);
 
 Factory.register(CargoFactory)
 
 if __name__ == '__main__':
-    from entity import Entity
-    print 'Subclass:', issubclass(CargoFactory, Factory)
-    print 'Instance:', isinstance(CargoFactory(), Factory)
+    from .entity import Entity
+    print('Subclass:', issubclass(CargoFactory, Factory))
+    print('Instance:', isinstance(CargoFactory(), Factory))
 
     cargo = CargoFactory().create(1, 5)
 
-    print 'Instance:', isinstance(cargo, Entity)
+    print('Instance:', isinstance(cargo, Entity))
