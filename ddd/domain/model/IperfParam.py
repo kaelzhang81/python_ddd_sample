262b54652be3587d2092c2ab366842c53e90e398
kaelzhang81 <zhangy1981@qq.com>
2017-11-29 12:45:35 +0800

    Update IperfParam.py

--- ddd/domain/model/IperfParam.py
+++ ddd/domain/model/IperfParam.py
@@ -1,7 +1,12 @@
-class IperfParam(object):
+# coding=utf-8
+
+import abc
+from .base.value_object import ValueObject
+
+class Iperf(object):
     def __init__(self, dest_ip, dest_port, report_interval=1,
                  duration=7200, bandwidth="50M",
-                 total_size=2000000, src_ip=None, isKeepAlive=False, numberOfBytes=None):
+                 total_size=2000000, src_ip=None)
         self.dest_ip = dest_ip
         self.dest_port = dest_port
         self.report_interval = report_interval
@@ -11,6 +16,19 @@ class IperfParam(object):
         self.buffer_len = 1024
         self.window_size = "512K"
         self.src_ip = src_ip
-        self.isKeepAlive = isKeepAlive
-        self.numberOfBytes = numberOfBytes
         self.parallel = 1
+
+    def send_udp(self):
+        cmd_result = self.ueServiceMgr.send_udp(self.__dict__)
+        if not cmd_result.result:
+            return None
+        return self._save_task_id(cmd_result)
+
+    def send_tcp(self):
+        cmd_result = self.ueServiceMgr.send_tcp(self.__dict__)
+        if not cmd_result.result:
+            return None
+        return self._save_task_id(cmd_result)
+
+ValueObject.register(Iperf)
+
