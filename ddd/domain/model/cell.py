02fd34119f212a0d86a5878ad4079ec1f448a5f5
kaelzhang81 <zhangy1981@qq.com>
2017-11-29 11:18:43 +0800

    Update cell.py

--- ddd/domain/model/cell.py
+++ ddd/domain/model/cell.py
@@ -12,45 +12,47 @@ from testlib.domain.model.cell.SignalRegulator import SignalRegulator
 from testlib.infrastructure.utility.envpara.EnvPara import EnvPara
 from testlib.infrastructure.utility.moctree.model.MoAgent import MoAgent
 from testlib.domain.model.cell.BandwidthRestrain import BandwidthRestrain
+import FunctionPolicy
+import SwitchPolicy
+
 from .base.aggregate_root import AggregateRoot
 
 
 class Cell(object):
 
-    def __init__(self, cell_alias, cp_list, band_width, freq):
-        self._signalRegulator = SignalRegulator(self)
-        self._kpiTask = None
-        self.functionPolicyDict = {}
-        self.switchPolicyDict = {}
+    def __init__(self, cell_alias, cp_list, band_width, freq, dm):
+        self._signal_regulator = SignalRegulator()
+        self._kpi_task = None
+        self.function_policy = FunctionPolicy()
+        self.switch_policy = SwitchPolicy()
         self._id = cell_alias
-        self._bandwidthRestrain = BandwidthRestrain(self)
+        self._bandwidth_restrain = BandwidthRestrain()
         self._cp_list = cp_list
         self._band_width = band_width
         self._freq = freq
-
+        self._dm = dm
 
     def role(self, className):
         return Role.convert(self, className)
 
     def shut_down(self):
         params = {'_iSubNetwork_': self._subNetId, '_iMEID_': self._meId, '_iEutrancellid_': self._id}
-        cmd_result = self.dm.query_cmd( params, 'SUCCESS')
+        cmd_result = self._dm.query_cmd( params, 'SUCCESS')
         if not cmd_result.result:
             raise Exception("failed to shut down cell")
     
     def start_up(self, query_times=18):
         params = {'_iSubNetwork_': self._subNetId, '_iMEID_': self._meId, '_iEutrancellid_': self._id}
-        cmd_result = self.dm.query(params, u'SUCCESS')
+        cmd_result = self._dm.query(params, u'SUCCESS')
         if not cmd_result.result:
             raise Exception("failed to start up cell ")
 
-
-    def get_rru_alias(self):
-        cp = self.cpList[0]
-        return cp.rru.boardName
+    def move_to_near_far(self, signal_value):
+        cp = self._cp_list[0]
+        return cp.move_to(signal_value)
 
 AggregateRoot.register(Cell)
 
 if __name__ == '__main__':
     print('Subclass:', issubclass(Cell, AggregateRoot))
-    print('Instance:', isinstance(Cell(1, [], 0, 0), AggregateRoot))
+    print('Instance:', isinstance(Cell(1, [], 0, 0, None), AggregateRoot))
