e992a08f550c833b76095f92913a9ac71d7b93bc
kaelzhang81 <zhangy1981@qq.com>
2017-12-01 17:14:35 +0800

    Update EnodebFactory.py

--- ddd/domain/model/EnodebFactory.py
+++ ddd/domain/model/EnodebFactory.py
@@ -1,17 +1,22 @@
 import Singleton
 import Enodeb
 import Factory
+import CcBoard
+import Bpl
+import FsBoard
+import RRU
 
 
 @Singleton
 class EnodebFactory(object):
 
     @staticmethod
-    def create(self, name, rrus, base_band_boards, fs_boards):
-        enodeb = Enodeb(name)
-        enodeb.add_device(rrus)
-        enodeb.add_device(base_band_boards)
-        enodeb.add_device(fs_boards)
+    def create(self, alias, cc, rru_list, bpl_list, fs_list):
+        enodeb = Enodeb(alias)
+        enodeb.add_device([CcBoard(cc)])
+        enodeb.add_device(RRU(x) for x in rru_list)
+        enodeb.add_device([Bpl(x) for x in bpl_list])
+        enodeb.add_device(FsBoard(x) for x in fs_list)
         return enodeb
 
 
